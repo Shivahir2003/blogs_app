e0922543fc1126920a54bd583303cfa0a6b6630e
Shivahir2003 <shivahir0114@gmail.com>
2024-07-25 21:00:35 +0530

    feat: created api for all models

--- blogsite/blogsite/urls.py
+++ blogsite/blogsite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('users/', include('accounts.urls')),
     path('blogs/', include('blogapp.urls')),
     path('api/', include('apiset.urls')),
