888f337353da39ed3db128464c6bea0d49b73e89
Shivahir2003 <shivahir0114@gmail.com>
2024-08-02 12:57:41 +0530

    fix : added filter setting and myblogs button  to header

--- blogsite/blogsite/urls.py
+++ blogsite/blogsite/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 
 from rest_framework import permissions
-
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from blogapp.views import BlogAppView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -36,6 +37,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('',BlogAppView.as_view()),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('accounts.urls')),
@@ -43,5 +45,4 @@ urlpatterns = [
     path('api/', include('apiset.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
