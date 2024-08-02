bb1c6d7e6e2603954c15fb457020bb8f8a67680d
Shivahir2003 <shivahir0114@gmail.com>
2024-08-02 12:53:37 +0530

    fix: minor changes

--- blogsite/blogapp/urls.py
+++ blogsite/blogapp/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from blogapp.views import BlogAppView
 
 app_name="blogapp"
+
 urlpatterns = [
+    path('',BlogAppView.as_view()),
     path('blog/dashboard',BlogAppView.as_view(), name="blog_dashboard"),
     path('blog/details/<int:pk>',BlogAppView.as_view(), name="blog_details"),
     path('blog/create',BlogAppView.as_view(), name="blog_create"),
--- blogsite/blogapp/utils.py
+++ blogsite/blogapp/utils.py
@@ -1,8 +1,14 @@
-from django.core.mail import send_mail,EmailMultiAlternatives
 from django.conf import settings
+from django.core.mail import send_mail
 
 
 def send_email(subject,message,user):
+    """
+        sending email to user 
+        
+        Arguments:
+            subject,message,user
+    """
     send_mail(
     subject,
     message,
